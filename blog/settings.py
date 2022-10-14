2cead20807bde1ce392393408a69d8e23e337974
David Akwuru <daakwuru@gmail.com>
2022-10-14 18:39:02 +0100

    first push to heruko

--- blog/settings.py
+++ blog/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config('DJANGO_SECRET_KEY')
+SECRET_KEY = "73siuk@n$yr&)_kg-bnu_t)ed=+q)*n7iq3s-s*snqrr3h9h!&"
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
@@ -151,8 +151,8 @@ X_FRAME_OPTIONS = 'SAMEORIGIN'
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_USE_SSL = True
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = config("EMAIL_HOST_USER")
-EMAIL_HOST_PASSWORD = config("EMAIL_HOST_PASSWORD")
+EMAIL_HOST_USER = "afariogun.john2002@gmail.com"
+EMAIL_HOST_PASSWORD = "fecdmgzpnvtiaksb"
 EMAIL_PORT = '465'
 
 django_heroku.settings(locals())
